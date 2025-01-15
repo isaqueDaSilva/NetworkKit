f331f79fe0e553936911140a25889a8db138ce60
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-01-15 04:53:29 -0300

    Package Description update with a new Common target

--- Package.swift
+++ Package.swift
@@ -17,20 +17,41 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkKit",
-            targets: ["NetworkHandler", "WebSocketHandler"]
+            targets: ["Common", "NetworkHandler", "WebSocketHandler"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.78.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkHandler"),
+            name: "Common"
+        ),
+        .testTarget(
+            name: "CommonTests",
+            dependencies: ["Common"]
+        ),
+        .target(
+            name: "NetworkHandler",
+            dependencies: [.target(name: "Common")]
+        ),
         .testTarget(
             name: "NetworkHandlerTests",
             dependencies: ["NetworkHandler"]
         ),
         .target(
-            name: "WebSocketHandler"),
+            name: "WebSocketHandler",
+            dependencies: [
+                .target(name: "Common"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOWebSocket", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio")
+            ]
+        ),
         .testTarget(
             name: "WebSocketHandlerTests",
             dependencies: ["WebSocketHandler"]

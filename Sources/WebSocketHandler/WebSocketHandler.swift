b6572669e69223ed6c55b4e2cf43c5da77b409e5
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-01-15 04:56:35 -0300

    Creating WebSocketHandler type.

--- Sources/WebSocketHandler/WebSocketHandler.swift
+++ Sources/WebSocketHandler/WebSocketHandler.swift
@@ -5,4 +5,23 @@
 //  Created by Isaque da Silva on 1/14/25.
 //
 
+import Common
 import Foundation
+import NIOCore
+import NIOPosix
+import NIOHTTP1
+import NIOWebSocket
+import NIOFoundationCompat
+
+public struct WebSocketHandler<ReceivedMessage: Decodable>: Sendable {
+    /// A representation path to find the desired path to open the connection
+    private let endpoint: Endpoint
+    
+    /// A default event loop group to make active the WebSocket connection.
+    private let eventLoopGroup: MultiThreadedEventLoopGroup
+    
+    /// A default WebSocket Upgrader for this aplication.
+    private var wsUpgrader: EventLoopFuture<UpgradeResult>?
+    
+    private var outboundWriter: NIOAsyncChannelOutboundWriter<WebSocketFrame>?
+}

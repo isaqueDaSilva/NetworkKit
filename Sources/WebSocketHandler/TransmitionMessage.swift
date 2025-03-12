19cfaa8ec9cac8f013819a173b773cc649cd528b
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-03-11 22:04:36 -0300

    Finishing the WebSocket Handler

--- Sources/WebSocketHandler/TransmitionMessage.swift
+++ Sources/WebSocketHandler/TransmitionMessage.swift
@@ -1,3 +1,10 @@
+//
+//  TransmitionMessage.swift
+//  NetworkKit
+//
+//  Created by Isaque da Silva on 3/11/25.
+//
+
 public struct TransmitionMessage: Sendable {
     let message: any Codable & Sendable
 }
--- Sources/WebSocketHandler/WSEndpoint.swift
+++ Sources/WebSocketHandler/WSEndpoint.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import NIOHTTP1
+
+struct WSEndpoint: Sendable {
+    internal let scheme: String
+    internal let host: String
+    internal let port: Int
+    internal let uri: String
+    internal var headers: HTTPHeaders
+    
+    init(
+        scheme: String,
+        host: String,
+        port: Int,
+        uri: String,
+        headers: HTTPHeaders
+    ) {
+        self.scheme = scheme
+        self.host = host
+        self.port = port
+        self.uri = uri
+        self.headers = headers
+    }
+}

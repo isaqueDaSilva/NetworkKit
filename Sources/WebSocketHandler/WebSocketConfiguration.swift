b08c9b0fadbb53afdb2126bef4cde34721aa640c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-03-12 21:30:38 -0300

    Creating a configurator type for WS Client type.

--- Sources/WebSocketHandler/WebSocketConfiguration.swift
+++ Sources/WebSocketHandler/WebSocketConfiguration.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
+//  WebSocketConfiguration.swift
 //  NetworkKit
 //
 //  Created by Isaque da Silva on 3/12/25.
 //
 
-import Foundation
+import struct Foundation.TimeInterval
+import NetworkHandler
+
+/// A representation data that enables the configuration process for ``WebSocketClient`` type.
+public struct WebSocketConfiguration {
+    let endpoint: Endpoint
+    let timeInterval: TimeInterval
+    let pingTryToReconnectCountLimit: Int
+    
+    public init(
+        endpoint: Endpoint,
+        timeInterval: TimeInterval = 20,
+        pingTryToReconnectCountLimit: Int = 3
+    ) {
+        self.endpoint = endpoint
+        self.timeInterval = timeInterval
+        self.pingTryToReconnectCountLimit = pingTryToReconnectCountLimit
+    }
+}

72a50afaec265188ff86fa2a7b0abbfc3b5da2c6
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-03-12 21:27:39 -0300

    Creation: WSConnectionState This is a data representation that represents the current state of a WebSocket channel.

--- Sources/WebSocketHandler/WSConnectionState.swift
+++ Sources/WebSocketHandler/WSConnectionState.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
+//  WSConnectionState.swift
 //  NetworkKit
 //
 //  Created by Isaque da Silva on 3/12/25.
 //
 
-import Foundation
+/// Representation of the current state of a WebSocket channel.
+public enum WSConnectionState: String {
+    case disconnected = "Disconnected"
+    case connecting = "Connecting"
+    case connected = "Connected"
+}
+
